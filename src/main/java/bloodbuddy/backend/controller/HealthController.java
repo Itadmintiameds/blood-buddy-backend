c95ce2e45b9235edd80496f598fe11b3d57d72ab
basavarajunn <basavarajunn@tiameds.ai>
2026-09-09 14:58:44 +0530

    Initial Spring Boot project

---
+++ src/main/java/bloodbuddy/backend/controller/HealthController.java
@@ -0,0 +1,15 @@
+package bloodbuddy.backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/public")
+public class HealthController {
+
+    @GetMapping("/health-check")
+    public String healthCheck() {
+        return "Service is up and running";
+    }
+}
